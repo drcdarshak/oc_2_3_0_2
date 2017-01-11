adb5293ffe19b6bbb2f480b551e955dce7089243
darshakshah <darshak.shah@drcinfotech.com>
2017-01-11 15:31:23 +0530

    Changes is connection files
    11-01-2017

--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://192.168.15.129/opencart/oc_2_3_0_2/');
+define('HTTP_SERVER', 'http://192.168.15.129/opencart/oc_2_3_0_git/oc_2_3_0_2/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://192.168.15.129/opencart/oc_2_3_0_2/');
+define('HTTPS_SERVER', 'http://192.168.15.129/opencart/oc_2_3_0_git/oc_2_3_0_2/');
 
 // DIR
-define('DIR_APPLICATION', 'D:/wamp/www/opencart/oc_2_3_0_2/catalog/');
-define('DIR_SYSTEM', 'D:/wamp/www/opencart/oc_2_3_0_2/system/');
-define('DIR_IMAGE', 'D:/wamp/www/opencart/oc_2_3_0_2/image/');
-define('DIR_LANGUAGE', 'D:/wamp/www/opencart/oc_2_3_0_2/catalog/language/');
-define('DIR_TEMPLATE', 'D:/wamp/www/opencart/oc_2_3_0_2/catalog/view/theme/');
-define('DIR_CONFIG', 'D:/wamp/www/opencart/oc_2_3_0_2/system/config/');
-define('DIR_CACHE', 'D:/wamp/www/opencart/oc_2_3_0_2/system/storage/cache/');
-define('DIR_DOWNLOAD', 'D:/wamp/www/opencart/oc_2_3_0_2/system/storage/download/');
-define('DIR_LOGS', 'D:/wamp/www/opencart/oc_2_3_0_2/system/storage/logs/');
-define('DIR_MODIFICATION', 'D:/wamp/www/opencart/oc_2_3_0_2/system/storage/modification/');
-define('DIR_UPLOAD', 'D:/wamp/www/opencart/oc_2_3_0_2/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/catalog/');
+define('DIR_SYSTEM', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/');
+define('DIR_IMAGE', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/image/');
+define('DIR_LANGUAGE', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/catalog/language/');
+define('DIR_TEMPLATE', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/config/');
+define('DIR_CACHE', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/storage/download/');
+define('DIR_LOGS', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/wamp/www/opencart/oc_2_3_0_git/oc_2_3_0_2/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
