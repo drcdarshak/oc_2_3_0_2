8c1717bc539c3c1c9be667f2f2b8c0161c1f3512
darshakshah <darshak.shah@drcinfotech.com>
2017-01-11 15:36:23 +0530

    Reinstall

--- admin/config.php
+++ admin/config.php
@@ -26,6 +26,6 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'darshak');
 define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'oc_2_3_0_2');
+define('DB_DATABASE', 'oc_2_3_0_git');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
